cec0da31b0903a233277091cdb32f8ed725e17cc
gleung93 <gleung93@gmail.com>
2015-04-05 00:13:36 -0700

    What I do Section

--- dist/assets/js/skill-spokes.js
+++ dist/assets/js/skill-spokes.js
@@ -1,64 +1,26 @@
-function hideUxSkills() {
-    var myElements = document.querySelectorAll(".skillux");
-    for (var i = 0; i < myElements.length; i++) {
-        myElements[i].style.left = "50%";
-        myElements[i].style.top = "50%";
-        myElements[i].style.opacity = "0";
-    }
-}
+$(document).ready(function(){
+    $('.skill-circle').mouseover(function(){
 
-
-function distributeUxSkills() {
-
-    var radius = 140;
-    var skills = $('.skillux') 
-    var container = $('.skill-container')
-    var width = container.width()
-    var height = container.height()
-    var angle = 0
-    var step = (2*Math.PI) / skills.length;
+        var radius = 140;
+        var skills = $(this).siblings()
+        var container = $('.skill-container')
+        var width = container.width()
+        var height = container.height()
+        var angle = 0
+        var step = (2*Math.PI) / skills.length;
     
-    skills.each(function() {
-        var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
-        var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
-        if(window.console) {
-            console.log($(this).text(), x, y);
-        }
-
-        $(this).css({"opacity": "1", "left": x+'px', "top": y+'px'});
-        angle += step;
+        skills.each(function() {
+            var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
+            var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
+            $(this).css({"opacity": "1", "left": x+'px', "top": y+'px'});
+            angle += step;
+        });
     });
-}
 
-function hideDevSkills() {
-    var myElements = document.querySelectorAll(".skilldev");
-    for (var i = 0; i < myElements.length; i++) {
-        myElements[i].style.left = "50%";
-        myElements[i].style.top = "50%";
-        myElements[i].style.opacity = "0";
-    }
-}
-
-
-function distributeDevSkills() {
-
-    var radius = 140;
-    var skills = $('.skilldev') 
-    var container = $('.skill-container')
-    var width = container.width()
-    var height = container.height()
-    var angle = 0
-    var step = (2*Math.PI) / skills.length;
-    
-    skills.each(function() {
-        var x = Math.round(width/2 + radius * Math.cos(angle) - $(this).width()/2);
-        var y = Math.round(height/2 + radius * Math.sin(angle) - $(this).height()/2);
-        if(window.console) {
-            console.log($(this).text(), x, y);
-        }
-
-        $(this).css({"opacity": "1", "left": x+'px', "top": y+'px'});
-        angle += step;
+    $('.skill-circle').mouseleave(function(){
+        $('.skill').css({"opacity": "0", "left": "50%", "top": "50%"});
     });
-}
+
+
+})
 
